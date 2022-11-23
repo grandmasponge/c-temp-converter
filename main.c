6c68d911746d69f27d3abfab13bea41c8cd27015
grandmasponge <tobyr2006@gmail.com>
2022-11-23 22:56:33 +0000

    commity numerous 3 it actually works tehe

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 struct myprogram
 {
-    char type[30];
+
     int temp;
     float temp_multiplier;
     float final_temp;
@@ -13,34 +13,40 @@ struct myprogram
 int main()
 {
    struct myprogram moby;
+   
+
+   char type[12];
 
    printf("celcuis to farenheight converter"); 
-   printf("if you want to convert celcuis to farenheight say farenheight \n is you want the other way round please say celcuis");
-   scanf("%s", moby.type);
-
-
-   if (moby.type == "celcuis") {
-
-    printf("input the number you want to convert into farenheight");
-    scanf("%s", moby.temp);
-    moby.final_temp = farenheight(moby.temp);
-    printf("your final temp is %f f", moby.final_temp);
-
-   } else if (moby.type == "farenheight") 
-   {
-    printf("input the number you want to convert into farenheight");
-    scanf("%s", moby.temp);
-    moby.final_temp = celcuis(moby.temp);
-    printf("your final temp is %f", moby.final_temp);
-   }
-   return 0;
+   printf("if you want to convert celcuis to farenheight say farenheight \n is you want the other way round please say celcuis:");
+   scanf("%s", type);
+   printf("%s", type);
+  
+  if (strcmp(type, "celcuis") == 0) {
+     printf("enter the temprature\n");
+     scanf("%d", &moby.temp);
+     moby.final_temp = celcuis(moby.temp);
+     printf("the final temp in celcuis is %f", moby.final_temp);
+     
+  }
+  else if (strcmp(type, "farenheight") == 0) {
+      printf("enter a temprature");
+      scanf("%d", &moby.temp);
+      moby.final_temp = farenheight(moby.temp);
+      printf(" your temp is: %f f", moby.final_temp);
+  }
+  
+  return 0;
 }
+
+
 
 int farenheight(moby) {
     struct myprogram coby;
     coby.temp_multiplier = 1.8;
-
-    coby.final_temp = moby * coby.temp_multiplier + 32;
+    int calculation;
+    calculation = moby * coby.temp_multiplier;
+    coby.final_temp = calculation + 32;
 
     return coby.final_temp;
 }
@@ -51,7 +57,7 @@ int celcuis(moby) {
     int calculation;
     coby2.temp_multiplier = 0.5556;
     calculation = moby - 32;
-    coby2.final_temp = calculation / coby2.temp_multiplier;
+    coby2.final_temp = calculation * coby2.temp_multiplier;
 
     return coby2.final_temp;
 }
